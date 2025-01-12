a93954cfea9f18c4c4d516570dd4523974896247
Azeem Munawar <raoazeemm@gmail.com>
2025-01-12 10:21:10 -0500

    revert changes

--- app/mailers/consultation_mailer.rb
+++ app/mailers/consultation_mailer.rb
@@ -1,10 +1,10 @@
 class ConsultationMailer < ApplicationMailer
-  default from: "meditechbilling.us@gmail.com"
+  default from: 'meditechbilling.us@gmail.com'
 
   def schedule_consultation(form_data)
     @form_data = form_data
-    mail(to: @form_data[:email], subject: "New Consultation Request") do |format|
-      format.html { render "schedule_consultation" }
+    mail(to: @form_data[:email], from: 'meditechbilling.us@gmail.com', subject: 'New Consultation Request') do |format|
+      format.html { render 'schedule_consultation' }
     end
   end
 end
