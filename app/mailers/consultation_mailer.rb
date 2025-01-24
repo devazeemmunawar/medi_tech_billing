43ce6caea8d0baa03ffca664759ce758b63d4db5
Azeem Munawar <raoazeemm@gmail.com>
2025-01-24 14:40:25 -0500

    Updated email address

--- app/mailers/consultation_mailer.rb
+++ app/mailers/consultation_mailer.rb
@@ -1,9 +1,9 @@
 class ConsultationMailer < ApplicationMailer
-  default from: 'meditechbilling.us@gmail.com'
+  default from: 'meditechbilling.usa@gmail.com'
 
   def schedule_consultation(form_data)
     @form_data = form_data
-    mail(to: 'meditechbilling.us@gmail.com', subject: 'New Consultation Request') do |format|
+    mail(to: 'meditechbilling.usa@gmail.com', subject: 'New Consultation Request') do |format|
       format.html { render 'schedule_consultation' }
     end
   end
